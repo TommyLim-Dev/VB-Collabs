9158488ced051d2d08393b9db74c29b433986edd
TommyLim-Dev <TommyLim-Dev@users.noreply.github.com>
2020-10-16 20:53:00 +0700

    Rename Form (to Main_Form)

--- GitHub/GitHub/Form1.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Form1
-    Private Sub Pertemuan1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Pertemuan1ToolStripMenuItem.Click
-
-    End Sub
-End Class
---
+++ GitHub/GitHub/Menu_Form.vb
@@ -0,0 +1,5 @@
+﻿Public Class Menu_Form
+    Private Sub Pertemuan1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Pertemuan1ToolStripMenuItem.Click
+
+    End Sub
+End Class
