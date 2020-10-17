d72a44ffab8330ceca892d242c8bd68429204b38
TommyLim-Dev <TommyLim-Dev@users.noreply.github.com>
2020-10-17 17:19:51 +0700

    Pertemuan 6 - Penjualan Barang

---
+++ GitHub/GitHub/pertemuan_06/Penjualan_Barang.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Penjualan_Barang
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(443, 419)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 31
+        Me.Button4.Text = "Next"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(314, 419)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 30
+        Me.Button3.Text = "Prev"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(183, 419)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 29
+        Me.Button2.Text = "View"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(41, 419)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 28
+        Me.Button1.Text = "Hapus"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Enabled = False
+        Me.TextBox6.Location = New System.Drawing.Point(183, 325)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox6.TabIndex = 27
+        Me.TextBox6.Text = "0"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(183, 258)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox5.TabIndex = 26
+        Me.TextBox5.Text = "0"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(183, 188)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox4.TabIndex = 25
+        Me.TextBox4.Text = "0"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(183, 119)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox3.TabIndex = 24
+        Me.TextBox3.Text = "0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(183, 59)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 23
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(183, 8)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 22
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(38, 328)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 17)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "Total Bayar"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(38, 261)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 17)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Diskon"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 191)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 17)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Jumlah"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 17)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Harga"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 17)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Nama Barang"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 17)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Data Ke -"
+        '
+        'Penjualan_Barang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(548, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Penjualan_Barang"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Penjualan_Barang"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ GitHub/GitHub/pertemuan_06/Penjualan_Barang.vb
@@ -0,0 +1,135 @@
+﻿Public Class Penjualan_Barang
+    Private Structure PenjualanBarang
+        Dim nama As String
+        Dim Harga As Double
+        Dim Jumlah As Double
+        Dim Diskon As Double
+        Dim total As Double
+    End Structure
+    Dim indeks As Integer
+    Dim data() As PenjualanBarang
+    Sub DaftarPenjualan()
+        data(indeks).nama = TextBox2.Text
+        data(indeks).Harga = TextBox3.Text
+        data(indeks).Jumlah = TextBox4.Text
+        data(indeks).Diskon = TextBox5.Text
+        data(indeks).Total = TextBox6.Text
+    End Sub
+    Sub HapusDaftar()
+        TextBox2.Text = ""
+        TextBox3.Text = "0"
+        TextBox4.Text = "0"
+        TextBox5.Text = "0"
+        TextBox6.Text = "0"
+        TextBox2.Focus()
+    End Sub
+
+    Sub tampilanData()
+        MsgBox("Informasi Penjualan Barang ke- " & indeks & Chr(10) &
+               "Nama Barang   : " & data(indeks).nama & Chr(10) &
+               "Harga Barang  : " & data(indeks).Harga & Chr(10) &
+               "Jumlah Barang : " & data(indeks).Jumlah & Chr(10) &
+               "Diskon : " & data(indeks).Diskon & Chr(10) &
+               "Total Bayar   : " & data(indeks).total, , "Penjualan Barang")
+    End Sub
+    Sub tampilanform()
+        TextBox2.Text = CStr(data(indeks).nama)
+        TextBox3.Text = CStr(data(indeks).Harga)
+        TextBox4.Text = CStr(data(indeks).Jumlah)
+        TextBox5.Text = CStr(data(indeks).Diskon)
+        TextBox6.Text = CStr(data(indeks).total)
+    End Sub
+
+
+    Private Sub Penjualan_Barang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        indeks = 1
+        ReDim data(indeks)
+        TextBox1.Text = indeks
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        HapusDaftar()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DaftarPenjualan()
+        tampilanData()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If indeks = 1 Then
+        ElseIf indeks > LBound(data) Then
+            DaftarPenjualan()
+            indeks = indeks - 1
+            tampilanform()
+        End If
+        If indeks = 0 Then indeks = 1
+        TextBox1.Text = indeks
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If indeks = UBound(data) Then
+            ReDim Preserve data(indeks + 1)
+        End If
+        TextBox1.Text = indeks
+        DaftarPenjualan()
+        indeks = indeks + 1
+        TextBox1.Text = indeks
+        tampilanform()
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        Dim total As String
+        Dim diskon As String
+        If TextBox3.Text = "" Then
+            TextBox3.Text = "0"
+        ElseIf TextBox4.Text = "" Then
+            TextBox4.Text = "0"
+        ElseIf TextBox5.Text = "" Then
+            TextBox5.Text = "0"
+        ElseIf TextBox6.Text = "" Then
+            TextBox6.Text = "0"
+        End If
+        total = (Val(TextBox3.Text * TextBox4.Text * TextBox5.Text))
+        diskon = total / 100
+        TextBox6.Text = total - diskon
+        TextBox6.Text = (Val(TextBox3.Text) * Val(TextBox4.Text) * Val(TextBox5.Text) / 100)
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        Dim total As Double
+        Dim diskon As Double
+        If TextBox3.Text = "" Then
+            TextBox3.Text = "0"
+        ElseIf TextBox4.Text = "" Then
+            TextBox4.Text = "0"
+        ElseIf TextBox5.Text = "" Then
+            TextBox5.Text = "0"
+        ElseIf TextBox6.Text = "" Then
+            TextBox6.Text = "0"
+        End If
+
+        total = (Val(TextBox3.Text * TextBox4.Text * TextBox5.Text))
+        diskon = total / 100
+        TextBox6.Text = total - diskon
+        TextBox6.Text = (Val(TextBox3.Text) * Val(TextBox4.Text) * Val(TextBox5.Text) / 100)
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+        Dim total As Double
+        Dim diskon As Double
+        If TextBox3.Text = "" Then
+            TextBox3.Text = "0"
+        ElseIf TextBox4.Text = "" Then
+            TextBox4.Text = "0"
+        ElseIf TextBox5.Text = "" Then
+            TextBox5.Text = "0"
+        ElseIf TextBox6.Text = "" Then
+            TextBox6.Text = "0"
+        End If
+
+        total = (Val(TextBox3.Text * TextBox4.Text))
+        diskon = total * (Val(TextBox5.Text) / 100)
+        TextBox6.Text = total - diskon
+    End Sub
+End Class
