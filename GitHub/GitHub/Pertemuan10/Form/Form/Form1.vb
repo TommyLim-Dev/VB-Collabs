31616b80d010a00e166d64ea4fb6554f285bc311
Nurfitra20 <72976901+Nurfitra20@users.noreply.github.com>
2020-10-20 20:42:57 +0700

    Pertemuan 10

---
+++ GitHub/GitHub/Pertemuan10/Form/Form/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Blogin_Click(sender As Object, e As EventArgs) Handles Blogin.Click
+
+        If CNN.State <> ConnectionState.Closed Then CNN.Close()
+        CNN.Open()
+        Dim OLECMD As OleDbCommand = New OleDbCommand("Select * FROM login WHERE username = '" & TextBox1.Text & "' and Password = '" & TextBox2.Text & "' ", CNN)
+        Dim OLERDR As OleDbDataReader = OLECMD.ExecuteReader
+        If (OLERDR.Read()) Then
+
+            MsgBox("sukses")
+            Form2.ShowDialog()
+        Else
+            MsgBox("Maaf Username dan Password tidak ditemukan....!", MsgBoxStyle.OkOnly, "Login Gagal")
+            TxtUser.Text = ""
+            TxtPassword.Text = ""
+            TxtUser.Focus()
+        End If
+    End Sub
+
+    Private Sub Bexit_Click(sender As Object, e As EventArgs) Handles Bexit.Click
+        End
+    End Sub
+End Class
---
+++ GitHub/GitHub/Pertemuan10/Form/Form/Module1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Data.OleDb
+Module Module1
+
+    Public Da As OleDbDataAdapter
+    Public Ds As DataSet
+    Dim LokasiDB As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data source=DBAplikasi.accdb"
+    Public CNN As OleDbConnection = New OleDbConnection(LokasiDB)
+
+
+End Module
