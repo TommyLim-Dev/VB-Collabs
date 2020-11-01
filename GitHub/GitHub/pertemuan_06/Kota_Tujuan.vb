d00bf92a283ef6dc79f24817696effab4af470dc
TommyLim-Dev <TommyLim-Dev@users.noreply.github.com>
2020-11-01 14:47:45 +0700

    Update Pertemuan 6

--- GitHub/GitHub/pertemuan_06/Kota_Tujuan.vb
+++ GitHub/GitHub/pertemuan_06/Kota_Tujuan.vb
@@ -1,8 +1,16 @@
 ﻿Public Class Kota_Tujuan
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim harga As Integer
         Dim Kota() As String = {"Jakarta", "Batam", "Bandung"}
         If Val(TextBox1.Text) > 0 AndAlso Val(TextBox1.Text) < 4 Then
-            MsgBox("Kota tujuan anda : " & Kota(Val(TextBox1.Text) - 1))
+            If TextBox1.Text = 1 Then
+                harga = 500000
+            ElseIf TextBox1.Text = 2 Then
+                harga = 450000
+            ElseIf TextBox1.Text = 3 Then
+                harga = 600000
+            End If
+            MsgBox("Kota tujuan anda : " & Kota(Val(TextBox1.Text) - 1) & vbNewLine & "Seharga = " & (harga * TextBox2.Text).ToString("N"))
         End If
     End Sub
 End Class
