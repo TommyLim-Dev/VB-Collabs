968abe56ab70c25c7f359b8e755749359441ac28
JuniusLindo191510030 <72977445+JuniusLindo191510030@users.noreply.github.com>
2020-11-27 21:00:26 +0700

    Add files via upload

---
+++ GitHub/GitHub/Pert 13/Pert 13/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Data belum lengkap, silahkan isi semua form !")
+        Else
+            Call Koneksi()
+            Cmd = New OleDbCommand("select * from Tb_user where Username='" & TextBox1.Text & "' and password='" & TextBox2.Text & "'", conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                Me.Hide()
+                Form2.Show()
+            Else
+                MsgBox("User atau Password salah!")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
