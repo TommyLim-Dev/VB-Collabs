e7f7a5ec904adf6a10e3681f5871d76b26c9de38
TommyLim-Dev <TommyLim-Dev@users.noreply.github.com>
2020-11-27 19:09:48 +0700

    UPdate

--- GitHub/GitHub/Pertemuan_08/poppeg.vb
+++ GitHub/GitHub/Pertemuan_08/poppeg.vb
@@ -1,40 +1,40 @@
-﻿Imports System.Data.OleDb
-Imports System.Data
-Public Class poppeg2
+﻿    Imports System.Data.OleDb
+    Imports System.Data
+    Public Class poppeg2
 
-    Private Sub clear_list()
+        Private Sub clear_list()
 
-    End Sub
+        End Sub
 
-    Private Sub txtNama_TextChanged(sender As Object, e As EventArgs) Handles txtNama.TextChanged
+        Private Sub txtNama_TextChanged(sender As Object, e As EventArgs) Handles txtNama.TextChanged
 
-    End Sub
-
-
-    Private Sub pilih()
+        End Sub
 
 
-    End Sub
-    Public Sub List_data()
-
-    End Sub
+        Private Sub pilih()
 
 
+        End Sub
+        Public Sub List_data()
 
-    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
-
-    End Sub
+        End Sub
 
 
-    Private Sub counter_TextChanged(sender As Object, e As EventArgs) Handles counter.TextChanged
 
-    End Sub
+        Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
 
-    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+        End Sub
 
-    End Sub
 
-    Private Sub poppeg_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Private Sub counter_TextChanged(sender As Object, e As EventArgs) Handles counter.TextChanged
 
-    End Sub
-End Class
+        End Sub
+
+        Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+
+        End Sub
+
+        Private Sub poppeg_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        End Sub
+    End Class
